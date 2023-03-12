3597685aaea7b5a33253eb37a6fb60df08cd90d2
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2023-03-12 21:47:32 +0600

    added bookings get api

--- index.js
+++ index.js
@@ -27,35 +27,57 @@ async function run() {
       .db("doctorService")
       .collection("bookings");
 
-      // use aggregate to query multiple collection and then to merge data
+    // use aggregate to query multiple collection and then to merge data
 
     app.get("/appointmentOptions", async (req, res) => {
       const query = {};
-      const date = req.query.date
-      console.log(date)
+      const date = req.query.date;
+      console.log(date);
       const options = await appointmentsOptionCollection.find(query).toArray();
 
       //get the bookings by the provided date
-      const bookingQuery = {appointmentDate:date}
-      const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray()
-
+      const bookingQuery = { appointmentDate: date };
+      const alreadyBooked = await bookingsCollection
+        .find(bookingQuery)
+        .toArray();
 
       // code carefully
-      options.forEach(option =>{
-        const optionBooked = alreadyBooked.filter(book=> book.treatment === option.name)
-        const bookedSlots = optionBooked.map(book=>book.slot)
-        const remainingSlots = option.slots.filter(slot=>!bookedSlots.includes(slot))
-        option.slots=remainingSlots
-      })
+      options.forEach((option) => {
+        const optionBooked = alreadyBooked.filter(
+          (book) => book.treatment === option.name
+        );
+        const bookedSlots = optionBooked.map((book) => book.slot);
+        const remainingSlots = option.slots.filter(
+          (slot) => !bookedSlots.includes(slot)
+        );
+        option.slots = remainingSlots;
+      });
       res.send(options);
     });
 
+    app.get("/bookings", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const booking = await bookingsCollection.find(query).toArray();
+      console.log(booking);
+      res.send(booking);
+    });
 
     app.post("/bookings", async (req, res) => {
-      const booking = req.body
-      
-      const result = await bookingsCollection.insertOne(booking)
-      console.log(result)
+      const booking = req.body;
+      const query = {
+        appointmentDate: booking.appointmentDate,
+        email: booking.email,
+        treatment: booking.treatment,
+      };
+      const alreadyBooked = await bookingsCollection.find(query).toArray();
+      if (alreadyBooked.length) {
+        const message = `You have already booked on ${booking.appointmentDate}`;
+        return res.send({ acknowledged: false, message });
+      }
+
+      const result = await bookingsCollection.insertOne(booking);
+      console.log(result);
       res.json(result);
     });
   } finally {
